15b80bf5c19271e5d01c2e91ba73c0a20a91f88f
ayman3000 <ayman3000@gmail.com>
2019-08-21 15:39:38 +0200

    added circular orbit.js file

---
+++ circular_orbit.js
@@ -0,0 +1,43 @@
+let theta = 0;
+let r = 200;
+let flag = true;
+
+function setup() {
+  createCanvas(600, 600);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(220);
+  
+  // Draw Earth's circle
+  translate(width/2, height/2);
+  fill('black');
+  noStroke();
+  circle(0,0,40);
+  
+  
+  // Draw the big hypothetical circle
+  noFill();
+  stroke('green');
+  strokeWeight(4);
+  circle(0,0,2 * r);
+  
+  // Draw Moon's circle
+  
+  x = r * cos(theta);
+  y = r * sin(theta);
+  
+  line(0,0,x,y);
+  noStroke();
+  fill('red');
+  circle(x,y,20);
+  theta++;
+}
+
+function mousePressed() {
+
+  if(flag) noLoop();
+  else loop();
+  flag = !flag;
+}
