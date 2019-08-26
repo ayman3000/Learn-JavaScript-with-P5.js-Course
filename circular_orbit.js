d1a675b4a22223bd32ac377c012874b153f685fe
ayman3000 <ayman3000@gmail.com>
2019-08-26 14:21:52 +0200

    added: circular_orbit

--- circular_orbit.js
+++ circular_orbit.js
@@ -10,20 +10,20 @@ function setup() {
 function draw() {
   background(220);
   
-  // Draw Earth's circle
+  // Draw nucleus circle
   translate(width/2, height/2);
   fill('black');
   noStroke();
   circle(0,0,40);
   
   
-  // Draw the big hypothetical circle
+  // Draw the orbit circle
   noFill();
   stroke('green');
   strokeWeight(4);
   circle(0,0,2 * r);
   
-  // Draw Moon's circle
+  // Draw electron circle
   
   x = r * cos(theta);
   y = r * sin(theta);
